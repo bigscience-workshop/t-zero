368be9527ad9e0ae416d14f63ea57fc67b4e03df
zheng-yanan <zyanan93@gmail.com>
2022-02-07 11:35:46 +0800

    Fix bug for evalution

--- evaluation/run_eval.py
+++ evaluation/run_eval.py
@@ -248,7 +248,16 @@ def main():
                 k: examples[k][i]
                 for k in column_names
             }
-            input, target = template.apply(ex)
+            # input, target = template.apply(ex)
+
+            ### Fix bug for several templates for super_glue/copa.
+            outputs = template.apply(ex)
+            if len(outputs) == 2:
+                input, target = outputs
+            else:
+                assert (len(outputs) == 1 and len(outputs[0]) == 0)
+                continue
+
             ex_answer_choices = template.get_answer_choices_list(ex)
             assert target in ex_answer_choices
             input_texts.append(input)
