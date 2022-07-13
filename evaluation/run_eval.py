dd93bb2ea680d8b462c5c6877b285b67cefdea52
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2022-07-13 16:17:55 +0200

    Hack my way so that 'None' string is considered like None

--- evaluation/run_eval.py
+++ evaluation/run_eval.py
@@ -312,7 +312,11 @@ def main():
     if args.dataset_name == "anli":
         raw_datasets = load_dataset(args.dataset_name, split=args.dataset_config_name)
     else:
-        raw_datasets = load_dataset(args.dataset_name, args.dataset_config_name, split="validation")
+        # TODO @thomasw21 hack!
+        if args.dataset_config_name == "None":
+            raw_datasets = load_dataset(args.dataset_name, split="validation")
+        else:
+            raw_datasets = load_dataset(args.dataset_name, args.dataset_config_name, split="validation")
     #TODO(Victor): enable loading pre-processed dataset from https://huggingface.co/datasets/bigscience/P3
 
     # Trim a number of evaluation examples
